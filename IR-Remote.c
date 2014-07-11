86d56171840a42341b889c1d239165195adcf4ca
eastWillow <bobgash2@gmail.com>
2014-07-11 17:38:41 +0800

    First IR Test

--- IR-Remote.c
+++ IR-Remote.c
@@ -1,2 +1,17 @@
 #include <STC12C5A60S2.H>
 #include <intrins.h>
+sbit IR = P1^3;
+void steup(){
+	CMOD = 0x0a;
+	CR = 0;
+	CCAPM0 = 0x42;
+	CCAP0L = 79;
+	CCAP0H = 79;
+}
+void main(){
+	steup();
+	while(1){
+		CR = 1;
+	}
+		
+}
